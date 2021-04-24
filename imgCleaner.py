d619648e466bac1b2ba946e964de8f7997c805f3
MrFish1604 <mmrfishc@gmail.com>
2021-04-24 15:52:43 +0200

    Allow user to specify file and epsilon

--- imgCleaner.py
+++ imgCleaner.py
@@ -2,6 +2,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 from math import sqrt
+from sys import argv
 
 def RMSPixel(pixel):
     summ = 0
@@ -14,10 +15,20 @@ def moyPixel(pixel):
         summ += val
     return summ/3
 
-img= Image.open("/home/matt/OneDrivePerso/graph.jpg")
+if len(argv)>1:
+    filename = argv[1]
+else:
+    print("Please specify a filename")
+    exit(0)
+
+if len(argv)>2:
+    epsilon = int(argv[2])
+else:
+    epsilon = 150
+
+img= Image.open(filename)
 ar = np.array(img)
 newImg = np.array(ar)
-epsilon = 150
 
 nbpix = 0
 for i in range(np.size(ar, 0)):
@@ -30,6 +41,8 @@ for i in range(np.size(ar, 0)):
 print(nbpix)
 plt.subplot(121)
 plt.imshow(newImg)
+plt.title("New")
 plt.subplot(122)
 plt.imshow(img)
+plt.title("Old")
 plt.show()
